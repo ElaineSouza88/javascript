9d0b0670a61f9572e0c150bc6f5f5de6f13ecd6b
Elaine Souza <elainehandball@yahoo.com.br>
2024-09-04 18:04:27 -0300

    Update aula 13
    Criação da estrutura de variáveis, aplicação do método para pegar hora local e parametrização das condições if / else.

--- aula13/script.js
+++ aula13/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function carregar() {
+    let msg = window.document.getElementById('msg') // irá exibir a mensagem que está dentro desse id.
+    let img = window.document.getElementById('imagem') // irá exibir a imagem que está dentro desse id.
+
+    let data = new Date() // pega a hora atual do Desktop.
+    let hora = data.getHours() // variável para exibir a data atual.
+
+    msg.innerHTML = `Agora são ${hora} horas !` // exibirá o conteúdo da variável hora.
+
+    if (hora >= 0 && hora < 12) {
+        //Bom dia !
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde !
+    } else {
+        //Boa noite!
+    }
+}
