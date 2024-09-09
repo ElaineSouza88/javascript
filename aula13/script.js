3235a76b386f5b5c71590241352af1249344e132
Elaine Souza <elainehandball@yahoo.com.br>
2024-09-09 13:12:25 -0300

    Update activits
    Aplicação das imagens de acordo com as horas do dia.

--- aula13/script.js
+++ aula13/script.js
@@ -1,17 +1,27 @@
 function carregar() {
     let msg = window.document.getElementById('msg') // irá exibir a mensagem que está dentro desse id.
+
     let img = window.document.getElementById('imagem') // irá exibir a imagem que está dentro desse id.
 
     let data = new Date() // pega a hora atual do Desktop.
-    let hora = data.getHours() // variável para exibir a data atual.
+
+    // let hora = data.getHours() // variável para exibir a data atual.
+
+    let hora = 19
 
     msg.innerHTML = `Agora são ${hora} horas !` // exibirá o conteúdo da variável hora.
 
     if (hora >= 0 && hora < 12) {
         //Bom dia !
-    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#FAEF2A'
+    } else if (hora >= 12 && hora <= 18) {
         //Boa tarde !
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#A5C4D7'
     } else {
         //Boa noite!
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#5F4C79'
     }
 }
