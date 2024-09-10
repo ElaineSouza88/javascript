164bd9a1e72b1598299909f99ef7baa229def5c6
Elaine Souza <elainehandball@yahoo.com.br>
2024-09-10 15:58:16 -0300

    Update script.js

--- aula14/script.js
+++ aula14/script.js
@@ -11,20 +11,43 @@ function verificar() {
         let fsex = document.getElementsByName('radsex')  // pega o Sexo do documento radsex, e armazena na variável fsex.
         let idade = ano - Number(fano.value) // armazena o resultado do ano atual - o ano preenchido pelo usuário.
         let genero = ''  // variável vazia para guardar o valor do índice.
-        let img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        let img = document.createElement('img') // cria uma tag para o img
+        img.setAttribute('id', 'foto') // cria um id para img
 
         //Segunda condição para marcação de masc ou fem
         if (fsex[0].checked) {  
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
-                //criança
+                //criança masculina
                 img.setAttribute('src', 'bebem.png')
+            } else if (idade < 21) {
+                //Adolescente
+                img.setAttribute('src', 'adolescentem.png')
+            } else if (idade < 45) {
+                //Homem
+                img.setAttribute('src', 'homem.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'avoh.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //criança feminina
+                img.setAttribute('src', 'bebef.png')
+            } else if (idade < 21) {
+                //Adolescente
+                img.setAttribute('src', 'adolescentef.png')
+            } else if (idade < 45) {
+                //Mulher
+                img.setAttribute('src', 'mulher.png')
+            } else {
+                // idosa
+                img.setAttribute('src', 'avo.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos !`
+        res.appendChild(img)  // adiciona um elemento de valor img e exibe o resultado na tela.
     }
 }
