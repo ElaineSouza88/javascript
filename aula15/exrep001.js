47d28341827eb791552cdf33d467a6d6487a6b20
Elaine Souza <elainehandball@yahoo.com.br>
2024-09-12 17:04:00 -0300

    Atualização dos exercícios do curso

---
+++ aula15/exrep001.js
@@ -0,0 +1,19 @@
+//Prátiaca para exercícios com estrutura de repetição enquanto
+
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+// console.log('Tudo bem? ')
+
+// let c = 1  // c é o contador que inicia a contagem em 1
+// while (c <= 6) {  // enquanto contador for menor ou = a 6
+//     console.log(`Passo ${c}`)
+//     c++ // é a mesma coisa que escrever c recebe c (c = e)
+// }
+
+let c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
